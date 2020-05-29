a825a7010f5dc5a7723829116febb3392a1a38b8
dev-appmonsters <dev.appmonsters@gmail.com>
2020-05-29 10:27:04 +0530

    fit: edit_post view to posts.py

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -15,10 +15,10 @@ def get_posts():
     posts = pagination.items
     prev = None
     if pagination.has_prev:
-        prev = url_for('api.get_posts', page=page-1, _external=True)
+        prev = url_for('api.get_posts', page=page - 1, _external=True)
     next = None
     if pagination.has_next:
-        next = url_for('api.get_posts', page=page+1, _external=True)
+        next = url_for('api.get_posts', page=page + 1, _external=True)
     return jsonify({
         'posts': [post.to_json() for post in posts],
         'prev': prev,
@@ -44,3 +44,13 @@ def new_post():
         {'Location': url_for('api.get_post', id=post.id, _external=True)}
 
 
+@api.route('/posts/<int:id>', methods=['PUT'])
+@permission_required(Permission.WRITE_ARTICLES)
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    if g.current_user != post.author and \
+            not g.current_user.can(Permission.ADMINISTER):
+        return forbidden('Insufficient permissions')
+    post.body = request.json.get('body', post.body)
+    db.session.add(post)
+    return jsonify(post.to_json())
