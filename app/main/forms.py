ac27a402a9423e4f0f6ade5f60f61c262a63e459
dev-appmonsters <dev.appmonsters@gmail.com>
2020-05-29 09:14:40 +0530

    fit: adding forms for using in veiws

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from flask_pagedown.fields import PageDownField
+from ..models import Role, User
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name?', vaidators=[Required()])
+    submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
